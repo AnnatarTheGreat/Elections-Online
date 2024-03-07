f016bea7ad88ebb1e27fdd63f92f5595c5677bf6
AnnatarTheGreat <rombik1552@yandex.ru>
2024-03-07 21:32:23 +0300

    test

--- Models/Ballot.cs
+++
@@ -1,8 +0,0 @@
-namespace PresidentSite.Models;
-
-public class Ballot : IBallot
-{
-    public int Id {get; set;}
-    public string LastName {get; set;}=null!;
-    public int Votes {get; set;}
-}
--- Models/IBallot.cs
+++
@@ -1,8 +0,0 @@
-namespace PresidentSite.Models;
-
-public interface IBallot
-{
-    int Id {get; set;}
-    string LastName {get; set;}
-    int Votes {get; set;}
-}
